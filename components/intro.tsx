edd633daf1661dfc624ec466ff9a910bf862a772
jorgegoco <jorgegoco70@gmail.com>
2023-09-20 14:26:06 +0200

    Add context for navigation bar

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,16 +1,30 @@
 'use client'
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import Image from 'next/image'
 import { motion } from 'framer-motion'
 import Link from 'next/link'
 import { BsArrowRight, BsLinkedin } from 'react-icons/bs'
 import { FaGithubSquare } from 'react-icons/fa'
 import { HiDownload } from 'react-icons/hi'
+import { useInView } from 'react-intersection-observer'
+import { useActiveSectionContext } from '@/context/active-section-context'
 
 export default function Intro() {
+  const [ref, inView] = useInView({
+    threshold: 0.50,
+  })
+  const { setActiveSection, timeOfLastClick } = useActiveSectionContext()
+
+  useEffect(() => {
+    if (inView && Date.now() - timeOfLastClick > 1000) {
+      setActiveSection("Home")
+    }
+  }, [inView, setActiveSection, timeOfLastClick])
   return (
     <section
+      ref={ref}
+      id="home"
       className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
     >
       <div className="flex items-center justify-center">
--- components/projects.tsx
+++ components/projects.tsx
@@ -1,14 +1,26 @@
 'use client';
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import SectionHeading from './section-heading'
 import { projectsData } from '@/lib/data'
 import Project from './project'
+import { useInView } from 'react-intersection-observer'
+import { useActiveSectionContext } from '@/context/active-section-context';
 
 export default function Projects() {
+  const [ref, inView] = useInView({
+    threshold: 0.50,
+  })
+  const { setActiveSection, timeOfLastClick } = useActiveSectionContext()
+
+  useEffect(() => {
+    if (inView && Date.now() - timeOfLastClick > 1000) {
+      setActiveSection("Projects")
+    }
+  }, [inView, setActiveSection, timeOfLastClick])
   
   return (
-    <section className='scroll-mt-28 mb-28'>
+    <section ref={ref} id="projects" className='scroll-mt-28 mb-28'>
       <SectionHeading>My projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
